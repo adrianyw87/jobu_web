47ca8917bc186174b96d6312e1fa204ac3156644
ComparteHuerto <adrianyellowworm@gmail.com>
2021-03-22 19:39:29 +0100

    maquetacion basica

--- jobuweb/karma.conf.js
+++ jobuweb/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jobuweb'),
+      dir: require('path').join(__dirname, './coverage/jobu'),
       subdir: '.',
       reporters: [
         { type: 'html' },
